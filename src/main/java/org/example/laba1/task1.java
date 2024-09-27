44ccbb083eea360141d342cf2fa48019c50ad0cb
vikasaharova <vikasaharok05@gmail.com>
2024-09-28 00:39:35 +0400

    java

---
+++ src/main/java/org/example/laba1/task1.java
@@ -0,0 +1,13 @@
+package org.example.laba1;
+public class task1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i);
+            if (i % 10 == 0) {
+                System.out.println();
+            } else {
+                System.out.print(", ");
+            }
+        }
+    }
+}
